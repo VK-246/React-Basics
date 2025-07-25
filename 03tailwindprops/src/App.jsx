c46da634bb7de6f73496339e47bfc5781a127c73
Vk-246 <vinayakkanhegaonkar5383@gmail.com>
2025-07-25 12:41:32 +0530

    Props and tailwind css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "tailwindcss";
+import Card from'./components/Card.jsx';
+import Card2 from'./components/Card2.jsx';
+function App() {
+  const details1 = {title:"real estates",btnTxt:"Deployed->"}
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center  min-h-screen gap-6 '>
+        <h1 className='bg-green-400 text-black p-4 rounded-4xl '>Tailwind test</h1>
+        <Card details={details1} />
+        <Card2 title="Accident Detection" btnTxt="Github ->" />
+        {/* <Card /> */}
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/components/Card2.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Card2({title="Title",btnTxt="Click Me->"}) {
+  // console.log("props:",props);
+     return(
+    <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
+        <img
+          src="https://images.pexels.com/photos/6099492/pexels-photo-6099492.jpeg"
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+        <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+          {title}
+        </span>
+          <h2 className="text-xl font-semibold tracking-wide">Lorem ipsum dolor</h2>
+        </div>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta
+          amet
+        </p>
+         <button className="px-4 mt-3 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 transition">
+          {btnTxt} 
+        </button>
+      </div>
+    )
+}
+
